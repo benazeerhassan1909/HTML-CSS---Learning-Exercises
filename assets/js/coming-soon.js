5e7bd020d7911e9ecfbd34782502c15f188e4b3d
benazeerhassan1909 <benazeer.hassan@multidots.com>
2021-11-09 22:57:21 +0530

    Recent Updates

---
+++ assets/js/coming-soon.js
@@ -0,0 +1,8 @@
+function validateEmail() {
+    console.log("swe");
+    // let x = document.forms["myForm"]["email"].value;
+    // const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // if (!re.test(String(email).toLowerCase())) {
+    //     alert("sdsd");
+    // }
+}
